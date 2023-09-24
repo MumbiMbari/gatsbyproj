df8280df2abe14e74eff17c6ee31506c7ea7e897
regina <mbariregina@gmail.com>
2023-09-24 10:46:59 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsbyproj`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
